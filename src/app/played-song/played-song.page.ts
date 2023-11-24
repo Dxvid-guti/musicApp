7bd8eb35e8273d8894941254b6acf0c4346b87a9
Dxvid-guti <degutierrez1024@gmail.com>
2023-11-24 11:00:02 -0500

    se agregaron animaciones en iconos de la pagina played-song

--- src/app/played-song/played-song.page.ts
+++ src/app/played-song/played-song.page.ts
@@ -5,7 +5,30 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './played-song.page.html',
   styleUrls: ['./played-song.page.scss'],
 })
+
 export class PlayedSongPage implements OnInit {
+
+  isIconHeartSelected = false;
+  isPlaying = false;
+  pushedBack = false;
+  pushedNext = false;
+
+  toggleIconHeart() {
+    this.isIconHeartSelected = !this.isIconHeartSelected;
+  }
+
+  togglePlayPause() {
+    this.isPlaying = !this.isPlaying;
+  }
+
+  togglePushedBack() {
+    this.pushedBack = !this.pushedBack;
+  }
+
+  togglePushedNext() {
+    this.pushedNext = !this.pushedNext;
+  }
+
 
   constructor() { }
 
