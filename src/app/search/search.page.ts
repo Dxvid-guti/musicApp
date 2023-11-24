e6844045e3c5ba223935d8b0317fc7e92bf1c67b
Dxvid-guti <degutierrez1024@gmail.com>
2023-11-24 07:37:48 -0500

    se finalizo la pagina de busqueda

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -9,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class SearchPage implements OnInit {
 
   songList: any[] = [
-    { numero: 1, nombre: 'Cancion 1', icono: 'play', imagen: './../../assets/antesAmeri.jpeg' },
-    { numero: 2, nombre: 'Cancion 2', icono: 'play', imagen: './../../assets/antesAmeri.jpeg' },
-    { numero: 3, nombre: 'Canción 3', icono: 'play', imagen: './../../assets/antesAmeri.jpeg' },
-    // Agrega más elementos según sea necesario
+    { numero: 1, nombre: 'CUANDO BAJE EL SOL', icono: 'ellipsis-horizontal-outline', imagen: './../../assets/saturno.jpg' },
+    { numero: 2, nombre: 'BESO', icono: 'ellipsis-horizontal-outline', imagen: './../../assets/beso.jpg' },
+    { numero: 3, nombre: 'LOVE', icono: 'ellipsis-horizontal-outline', imagen: './../../assets/love.jpg' },
+    { numero: 4, nombre: 'Sola (Remix)', icono: 'ellipsis-horizontal-outline', imagen: './../../assets/sola.jpg' },
+    { numero: 5, nombre: 'Tokyo', icono: 'ellipsis-horizontal-outline', imagen: './../../assets/tokyo.jpg' },
+    { numero: 6, nombre: 'Obsesionado', icono: 'ellipsis-horizontal-outline', imagen: './../../assets/obsesionado.jpg' },
   ];
 
   constructor() { }
